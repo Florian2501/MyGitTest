3b9ce76f0a26129c7cb3a007ae81cc3c66332450
Florian2501 <florischierz@gmail.com>
2021-05-18 10:56:03 +0200

    Revert "Adds a new output line"
    This reverts commit 97ad9ea6f77d65ccec6ddd9611725da0d4e746b5.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,7 +7,6 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            System.Console.WriteLine("Das ist eine weitere Zeile zum Test.");
             System.Console.WriteLine("Diese Line soll aber bleiben...");
             System.Console.WriteLine("Diese Line aber eigentlich auch...");
             System.Console.WriteLine("Und was passiert jetzt?");
