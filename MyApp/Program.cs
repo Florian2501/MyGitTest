4bc037d04f0f8568c1417fe3d4873c8380801924
Florian2501 <florischierz@gmail.com>
2021-05-18 12:01:40 +0200

    Adds Comments relates to #2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,13 +6,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            //Gives an output to proof functionality
             Console.WriteLine("Hello World!");
             System.Console.WriteLine("Lets try wether this will merge");
             System.Console.WriteLine("ups die sollte doch auch noch mit rein!");
             System.Console.WriteLine("Diese Line soll aber bleiben...");
             System.Console.WriteLine("Diese Line aber eigentlich auch...");
             System.Console.WriteLine("Und was passiert jetzt?");
+            //calls the function below
             printHello();
+            //calls the function from another file
             PrintClass.printSomething();
         }
 
