5d804e00b04a1bc80e22dc8836b079d3001e11e2
Florian2501 <florischierz@gmail.com>
2021-05-18 10:51:08 +0200

    Adds line in master to test wether merging works with dev branch

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,6 +7,8 @@ namespace MyApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Lets try wether this will merge");
+            System.Console.WriteLine("ups die sollte doch auch noch mit rein!");
         }
     }
 }
