d4ae2d72d61c799b978bcf5df62cd49c04856053
Florian2501 <florischierz@gmail.com>
2021-05-18 10:49:10 +0200

    Adds 3 more output lines.

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,6 +8,9 @@ namespace MyApp
         {
             Console.WriteLine("Hello World!");
             System.Console.WriteLine("Das ist eine weitere Zeile zum Test.");
+            System.Console.WriteLine("Diese Line soll aber bleiben...");
+            System.Console.WriteLine("Diese Line aber eigentlich auch...");
+            System.Console.WriteLine("Und was passiert jetzt?");
         }
     }
 }
