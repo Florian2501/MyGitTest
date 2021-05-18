811f6036535a983d4bff8c90c5b5a6f41dedef7d
Florian2501 <florischierz@gmail.com>
2021-05-18 12:08:29 +0200

    Adds the comments for the 2. File
    relates to #2

--- MyApp/PrintClass.cs
+++ MyApp/PrintClass.cs
@@ -1,6 +1,7 @@
 namespace MyApp{
         public static class PrintClass{
 
+//prints just the word "something"
             public static void printSomething(){
                 System.Console.WriteLine("Something...");
             }
